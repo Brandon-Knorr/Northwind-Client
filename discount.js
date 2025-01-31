3dbd0b3470961c1f5f80ef29f455caa85fc2ed1a
Brandon-Knorr <bknorr@my.wctc.edu>
2025-01-30 18:20:27 -0600

    [2.07] Delegate listener using discount class

--- discount.js
+++ discount.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById('discount-row').addEventListener('click', function(e){
+      if (e.target.classList.contains('discount')) {
         e.preventDefault();
-    bootstrap.Toast.getOrCreateInstance(document.getElementById('liveToast')).show();
+        bootstrap.Toast.getOrCreateInstance(document.getElementById('liveToast')).show();
+      }
     });
-});
+  });
