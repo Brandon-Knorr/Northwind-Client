e43c2848d93f4699dab9fe7ba8c5241291c1883b
Brandon-Knorr <bknorr@my.wctc.edu>
2025-02-06 17:11:32 -0600

    [2.19] finish datasets for toast

--- discount.js
+++ discount.js
@@ -7,3 +7,23 @@ document.addEventListener("DOMContentLoaded", function() {
       }
     });
   });
+
+  document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById('discount-row').addEventListener('click', function(e){
+      if (e.target.classList.contains('discount')) {
+        e.preventDefault();
+        document.getElementById('discount-title').innerHTML = e.target.dataset['title'];
+        bootstrap.Toast.getOrCreateInstance(document.getElementById('liveToast')).show();
+      }
+    });
+  }); 
+
+  document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById('discount-row').addEventListener('click', function(e){
+      if (e.target.classList.contains('discount')) {
+        e.preventDefault();
+        document.getElementById('code').innerText = e.target.dataset['code'];
+        bootstrap.Toast.getOrCreateInstance(document.getElementById('liveToast')).show();
+      }
+    });
+  }); 
