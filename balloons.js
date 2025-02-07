e3e8233fa2b229390c86d68a710c90239b33905c
Brandon-Knorr <bknorr@my.wctc.edu>
2025-02-06 18:25:40 -0600

    [3.09e] Remove css animation classes to enable repeated animations

--- balloons.js
+++ balloons.js
@@ -1,19 +1,22 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const elem = document.getElementById('dob');
-    const datepicker = new Datepicker(elem, {
-      // options
-      autohide: true,
-      format: 'MM-dd'
-    });
-
-      // uncheck all boxes by default (Firefox)
-    document.querySelectorAll('.form-check-input').forEach(c => c.checked = false);
-      // event listener for check/uncheck
-    document.getElementById('checkbox-card').addEventListener('change', function(e){
-      if (e.target.classList.contains('form-check-input')) {
-        console.log(e);
-        document.getElementById(e.target.id + 'Img').style.visibility = e.target.checked ? "visible" : "hidden";
-      }
-    });
-
+  const elem = document.getElementById('dob');
+  const datepicker = new Datepicker(elem, {
+    // options
+    autohide: true,
+    format: 'MM-dd'
   });
+
+  // uncheck all boxes by default (Firefox)
+  document.querySelectorAll('.form-check-input').forEach(c => c.checked = false);
+  // event listener for check/uncheck
+  document.getElementById('checkbox-card').addEventListener('change', function(e){
+    if (e.target.classList.contains('form-check-input')) {
+      const elem = document.getElementById(e.target.id + 'Img');
+      elem.style.visibility = "visible";
+      elem.classList.remove("animate__animated", "animate__bounceInDown", "animate__bounceOutUp");
+      e.target.checked ?
+        elem.classList.add("animate__animated", "animate__bounceInDown") :
+        elem.classList.add("animate__animated", "animate__bounceOutUp");
+    }
+  });
+});
